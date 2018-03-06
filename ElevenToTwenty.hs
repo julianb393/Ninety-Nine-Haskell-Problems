4f2569474104d106bd8a6dcf5477503688fa7efc
Julian Barker <julian.b@live.ca>
2018-03-06 17:49:56 -0500

    Completed problems 11 to 20.

--- ElevenToTwenty.hs
+++ ElevenToTwenty.hs
@@ -23,3 +23,53 @@ decodeModified [] = []
 decodeModified (y:ys) = (decodePiece y) ++ (decodeModified ys)
   where decodePiece (Single x) = [x]
         decodePiece (Multiple times x) = replicate times x
+
+-- Problem 13 Run-length encoding of a list (direct solution).
+encodeDirect :: (Eq a) => [a] -> [Quantity Int a]
+encodeDirect [] = []
+encodeDirect lst@(x:xs) = let r = (takeW lst) in
+  [encoded r] ++ encodeDirect (drop (length r) lst)
+    where encoded elst
+            |length elst == 1 = Single (head elst)
+            |otherwise = Multiple (length elst) (head elst)
+          takeW (x:xs) = takeWhile (== x) (x:xs)
+
+-- Problem 14 Duplicate the elements of a list.
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = [x, x] ++ (dupli xs)
+
+-- Problem 15 Replicate the elements of a list a given number of times.
+repli :: [a] -> Int -> [a]
+repli [] _ = []
+repli (x:xs) i = (replicate i x) ++ (repli xs i)
+
+-- Problem 16 Drop every N'th element from a list.
+dropEvery :: [a] -> Int -> [a]
+dropEvery lst i
+  |(length lst) < i = lst
+  |otherwise = (init $ take i lst) ++ (dropEvery (drop i lst) i)
+  -- For otherwise condition, could have just done take i-1 ...
+
+-- Problem 17 Split a list into two parts;
+-- the length of the first part is given.
+split :: [a] -> Int -> ([a], [a])
+split lst i = (take i lst, drop i lst)
+
+-- Problem 18 Extract a slice from a list.
+slice :: [a] -> Int -> Int -> [a]
+slice lst i k = take (k - i + 1) $ drop (i - 1) lst
+
+-- Problem 19 Rotate a list N places to the left.
+rotate :: [a] -> Int -> [a]
+rotate lst i
+  |i < 0 = (drop ((length lst) + i) lst) ++ (take ((length lst) + i) lst)
+  |i == 0 = lst
+  |otherwise = (drop i lst) ++ (take i lst)
+
+-- Problem 20 Remove the K'th element from a list.
+removeAt :: Int -> [a] -> (a, [a])
+removeAt i lst = case back of
+  [] -> error "Index out of bounds."
+  (x:xs) -> (x, front ++ xs)
+  where (front, back) = splitAt (i - 1) lst
