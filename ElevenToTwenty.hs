a8baa2eb7ee5bb476c7364e3ecdc6cc7764bf490
Julian Barker <julian.b@live.ca>
2018-03-06 01:32:19 -0500

    Fixed indentation in OneToTen.hs and created new file ElevenToTwenty.hs.

---
+++ ElevenToTwenty.hs
@@ -0,0 +1,9 @@
+-- @author: Julian Barker
+-- My own solutions to Problem 11 - 20. Please note that there
+-- are various ways of implementing these functions.
+
+module ElevenToTwenty where
+
+import OneToTen
+
+  fcn lst = pack lst
