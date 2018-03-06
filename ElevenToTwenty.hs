8fd27a84c0db5aabcf1c98eae2cef985019f93fc
Julian Barker <julian.b@live.ca>
2018-03-06 02:07:03 -0500

    Completed questions 11 and 12.

--- ElevenToTwenty.hs
+++ ElevenToTwenty.hs
@@ -6,4 +6,20 @@ module ElevenToTwenty where
 
 import OneToTen
 
-  fcn lst = pack lst
+data Quantity a b = Single b | Multiple a b
+  deriving Show
+
+-- Problem 11 Modified run-length encoding.
+encodeModified :: (Eq a) => [a] -> [Quantity Int a]
+encodeModified lst = map encodePiece pLst
+  where pLst = pack lst
+        encodePiece piece
+          |length piece == 1 = Single (head piece)
+          |otherwise = Multiple (length piece) (head piece)
+
+-- Problem 12 Decode a run-length encoded list.
+decodeModified :: [Quantity Int a] -> [a]
+decodeModified [] = []
+decodeModified (y:ys) = (decodePiece y) ++ (decodeModified ys)
+  where decodePiece (Single x) = [x]
+        decodePiece (Multiple times x) = replicate times x
